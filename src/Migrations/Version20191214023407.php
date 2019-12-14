c60d71d2b2fe7efbe61078cd851c7e912cfee47b
accessiproject <kevin.bustamante@mail.novancia.fr>
2019-12-14 03:36:29 +0100

    version du projet avec authentification initiale

--- src/Migrations/Version20191214023407.php
+++ src/Migrations/Version20191214023407.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191211123858 extends AbstractMigration
+final class Version20191214023407 extends AbstractMigration
 {
     public function getDescription() : string
     {
