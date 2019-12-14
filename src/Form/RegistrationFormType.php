27fbd6e2334fb8725c88d3ff4f440496d18d9281
accessiproject <kevin.bustamante@mail.novancia.fr>
2019-12-14 06:42:53 +0100

    contrainte invalidite email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
@@ -23,13 +25,18 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('email', TextType::class, [
                 'required' => false,
+                'constraints' => [
+                    new Email([
+                        'message' => 'Le champ «adresse email» est invalide.',
+                    ]),
+                ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'required' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Vous devez cocher les conditions.',
+                        'message' => 'Vous devez accepter les conditions.',
                     ]),
                 ],
             ])
